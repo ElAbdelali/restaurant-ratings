e0931d1d3cead692e849a3d02c78f3b57fed252f
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-10 15:05:38 -0500

    Completed ratings.py

--- ratings.py
+++ ratings.py
@@ -1,4 +1,27 @@
 """Restaurant rating lister."""
+scores = {}
 
 
-# put your code here
+def restaurant_ratings(filename):
+    #key: restaurant
+    #value: ratings
+    for line in open(filename):
+        restaurant, ratings = line.rstrip().split(":")
+        scores[restaurant] = ratings
+        # print(f"{restaurant} is rated at {ratings}.")
+
+
+def add_ratings():
+    restaurant = input("Please enter the restaurant name? ")
+    ratings = input("Please enter the rating? ")
+    scores[restaurant] = ratings
+
+
+def show_ratings():
+    for restaurant, ratings in sorted(scores.items()):
+        print(f"{restaurant} is rated at {ratings}.")
+
+
+add_ratings()
+restaurant_ratings('scores.txt')
+show_ratings()
